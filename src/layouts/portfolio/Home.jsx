4c29078b918bfe14b3cc35e5cc31ae644c093985
Temiloluwa <ogunbanjotemiloluwa@gmail.com>
2023-07-07 12:00:04 +0100

    Added some changes

--- src/layouts/portfolio/Home.jsx
+++ src/layouts/portfolio/Home.jsx
@@ -824,7 +824,7 @@ const PortfolioIndex = () => {
                 deletionSpeed={88}
                 cursor={true}
                 repeat={0}
-                className="user-details text-justify uppercase"
+                className="user-details text-justify"
                 style={{
                   fontFamily: "monospace",
                   fontSize: "14px",
